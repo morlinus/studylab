e93af8f40ef72856659cd12144271be86a925447
Linus Morlinghaus <lm092@hdm-stuttgart.de>
2018-10-27 20:40:16 +0200

    registrierung mit der Datenbank verbdunden und alle geplanten funktion eingebaut. Nun bis auf hash funktion, funktionstüchtig

--- registrierung.php
+++ registrierung.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+include 'userdata.php'; //anstatt $pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,23 +17,64 @@ if(isset($_GET['register'])) {
     $email = $_POST['email'];
     $passwort = $_POST['passwort'];
     $passwort2 = $_POST['passwort2'];
+    $benutzername = $_POST ['benutzername'];
+    $name = $_POST ['name'];
+    $nachname = $_POST ['nachname'];
+    $geburtsdatum = $_POST ['geburtsdatum'];
+    $studiengang = $_POST ['studiengang'];
+    $geschlecht = $_POST ['geschlecht'];
+    $semester = $_POST ['semester'];
+    $status = $_POST ['status'];
+
 
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
         $error = true;
     }
-    if(strlen($passwort) == 0) {
+    if(strlen($passwort) == 0) { // Überprüfung, ob eingabe getätigt
         echo 'Bitte ein Passwort angeben<br>';
         $error = true;
     }
-    if($passwort != $passwort2) {
+    if($passwort != $passwort2) { // überprüfung, ob passwörter identisch sind
         echo 'Die Passwörter müssen übereinstimmen<br>';
         $error = true;
     }
+    if(strlen($benutzername) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }
+    if(strlen($name) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }
+    if(strlen($nachname) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }if(strlen($geburtsdatum) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }
+    if(strlen($studiengang) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }
+    if(strlen($geschlecht) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }
+    if(strlen($semester) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }
+    if(strlen($status) == 0) {
+        echo 'Bitte einen Benutzernamen angeben<br>';
+        $error = true;
+    }
 
-    //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+
+    //Hier wird überprüft, ob die E-Mail-Adresse noch nicht registriert wurde
     if(!$error) {
-        $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+        $statement = $pdo->prepare("SELECT * FROM studylab WHERE email = :email");
         $result = $statement->execute(array('email' => $email));
         $user = $statement->fetch();
 
@@ -43,12 +84,13 @@ if(isset($_GET['register'])) {
         }
     }
 
-    //Keine Fehler, wir können den Nutzer registrieren
+    //wenn keine Fehler vorliegen, wird hier der Nutzer registriert
     if(!$error) {
-        $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+       // $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT); //passwort hashen
 
-        $statement = $pdo->prepare("INSERT INTO users (email, passwort) VALUES (:email, :passwort)");
-        $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash));
+        $statement = $pdo->prepare("INSERT INTO studylab (email, passwort,benutzername, name, nachname, geburtsdatum, studiengang, geschlecht, semester, status) VALUES (:email, :passwort, :benutzername, :name, :nachname, :geburtsdatum, :studiengang, :geschlecht, :semester, :status)");
+        //$result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash, 'benutzername' => $benutzername, 'name' => $name, 'nachname' => $nachname, 'geburtsdatum' => $geburtsdatum, 'studiengang' => $studiengang, 'geschlecht' => $geschlecht, 'semester' => $semester,'status' => $status));
+        $result = $statement->execute(array('email' => $email, 'passwort' => $passwort, 'benutzername' => $benutzername, 'name' => $name, 'nachname' => $nachname, 'geburtsdatum' => $geburtsdatum, 'studiengang' => $studiengang, 'geschlecht' => $geschlecht, 'semester' => $semester,'status' => $status));
 
         if($result) {
             echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
@@ -63,8 +105,30 @@ if($showFormular) {
     ?>
 
     <form action="?register=1" method="post">
+        Vorname:<br>
+        <input type="name" size="40" maxlength="250" name="name"><br><br>
+        Nachname:<br>
+        <input type="nachname" size="40" maxlength="250" name="nachname"><br><br>
+
+        Benutzername: <br>
+        <input type="benutzername" size 40 maxlength="50" name="benutzername"><br><br>
         E-Mail:<br>
         <input type="email" size="40" maxlength="250" name="email"><br><br>
+
+        Geburtsdatum: <br>
+        <input type="geburtsdatum" size 40 maxlength="50" name="geburtsdatum"><br><br>
+
+        Studiengang: <br>
+        <input type="studiengang" size 40 maxlength="50" name="studiengang"><br><br>
+
+        Geschlecht: <br>
+        <input type="geschlecht" size 40 maxlength="50" name="geschlecht"><br><br>
+
+        Semester: <br>
+        <input type="semester" size 40 maxlength="50" name="semester"><br><br>
+
+        Position: <br>
+        <input type="status" size 40 maxlength="50" name="status"><br><br>
 
         Dein Passwort:<br>
         <input type="password" size="40"  maxlength="250" name="passwort"><br>
