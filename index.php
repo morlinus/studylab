d1bf0d243158a44fb3f9fc1531eef232bf4d6ac1
anni325 <as325@hdm-stuttgart.de>
2018-10-27 19:55:52 +0200

    logout und index

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ session_start();
 <html lang="de">
 <head>
     <meta charset="utf-8">
+
+    //stylesheet
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
--- login.php
+++ login.php
@@ -1,25 +1,33 @@
 <?php
- session_start();
+session_start();
 
- include 'userdata.php';
-
-if(isset($_GET['login'])) {
-    $email = $_POST['email'];
-    $passwort = $_POST['passwort'];
-
-    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-    $result = $statement->execute(array('email' => $email));
-    $user = $statement->fetch();
-
-    //Überprüfung des Passworts
-    if ($user !== false && password_verify($passwort, $user['passwort'])) {
-        $_SESSION['userid'] = $user['id'];
-        die('Login erfolgreich. Weiter zu <a href="index.php">Startseite</a>');
-    } else {
-        $errorMessage = "E-Mail oder Passwort war ungültig<br>";
-    }
-
+if(isset($_POST["email"]) AND isset($_POST["passwort"]))
+{
+    $email=$_POST["email"];
+    $passwort=$_POST["passwort"];
 }
+
+
+include 'userdata.php';
+
+$statement =$pdo->prepare("SELECT * FROM studylab WHERE email=:email AND passwort=:passwort");
+if($statement->execute(array(':email'=>$email, ':passwort'=>$passwort))) {
+    if ($row=$statement->fetch()) {
+        echo "Login erfolgreich.";
+        $_SESSION["angemeldet"]=$row["id"];
+        //header(Location: index.php);
+    }
+    else
+    {
+        echo "Melde dich an!";
+    }
+} else {
+    echo "Datenbank-Fehler:";
+    echo $statement->errorInfo()[2];
+    echo $statement->queryString;
+    die();
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,18 +36,12 @@ if(isset($_GET['login'])) {
 </head>
 <body>
 
-<?php
-if(isset($errorMessage)) {
-    echo $errorMessage;
-}
-?>
-
 <form action="?login=1" method="post">
-    E-Mail:<br>
+    Email:<br>
     <input type="email" size="40" maxlength="250" name="email"><br><br>
 
     Dein Passwort:<br>
-    <input type="password" size="40"  maxlength="250" name="passwort"><br>
+    <input type="passwort" size="40"  maxlength="250" name="passwort"><br>
 
     <input type="submit" value="Abschicken">
 </form>
