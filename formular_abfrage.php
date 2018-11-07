842b8157dadd9acc13f6df216038ce2ad208e6ae
anni325 <as325@hdm-stuttgart.de>
2018-11-07 11:09:52 +0100

    Formular Abfrage

---
+++ formular_abfrage.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+include 'userdata.php';
+
+$id = $_SESSION["id"];
+
+if(!isset($_SESSION["angemeldet"]))
+{
+    echo"nicht angemeldet.";
+    die();
+}
+
+$content= $_POST["content"];
+
+$statement = $pdo->prepare("INSERT INTO content VALUES ('',?,?,?)");
+$statement->execute(array(':text'=>$content, ':userid'=>$id));
+echo "id in der Datenbank: ".$neueid=$pdo->lastInsertId();
+
+
+
+
