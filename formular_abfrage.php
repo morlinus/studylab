1639103a52bccf128e65ef639e83d904dd624026
Linus Morlinghaus <lm092@hdm-stuttgart.de>
2018-11-07 12:51:16 +0100

    Merge branch 'master' of /Users/linusmorlinghaus/Documents/studylab with conflicts.

--- formular_abfrage.php
+++ formular_abfrage.php
@@ -3,9 +3,7 @@
 session_start();
 include 'userdata.php';
 
-
 $id = $_SESSION["id"];
-
 
 if(!isset($_SESSION["angemeldet"]))
 {
@@ -15,8 +13,8 @@ if(!isset($_SESSION["angemeldet"]))
 
 $content= $_POST["content"];
 
-$statement = $pdo->prepare("INSERT INTO content VALUES ('',:userid,:text)");
-$statement->execute(array(':text'=>$content, ':userid'=>$id));
+$statement = $pdo->prepare("INSERT INTO content VALUES ('',?,?,?)");
+$statement-> execute(array('text'=>$content, 'userid'=>$id));
 echo "id in der Datenbank: ".$neueid=$pdo->lastInsertId();
 
 
