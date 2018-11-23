2ffcc55402a330302fcb264c4f7b35b1e47c506e
Linus Morlinghaus <lm092@hdm-stuttgart.de>
2018-11-23 09:05:59 +0100

    index mkt header versehen

--- index.php
+++ index.php
@@ -1,48 +1,14 @@
+<?php
+include_once 'header.php';
+?>
+
 <!doctype html>
 <html lang="de">
 <head>
-
-    <meta charset="utf-8">
-<!-- Fügt den Namen der eingeloggten Person in den Titel ein -->
 <title>Startseite</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-    <link href="studylab.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user- scalable=yes">
-
-    <!--verschiebt den Conatiner an den rechten Seitenrand -->
-
 </head>
 <body>
-<div id="header">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">StudiLAB</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="https://mars.iuk.hdm-stuttgart.de/~as325/index.php">Startseite</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="https://mars.iuk.hdm-stuttgart.de/~as325/login.php">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://mars.iuk.hdm-stuttgart.de/~as325/logout.php">Logout</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://mars.iuk.hdm-stuttgart.de/~as325/nutzerprofil.php">Profil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://mars.iuk.hdm-stuttgart.de/~as325/impressum.html">Impressum</a>
-            </li>
-        </ul>
-    </div>
-</nav>
     <div class="container">
         <div class="row">
             <div class="col-md-4">
