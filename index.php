16657732c7f38e3bb38603f67409ee7da46f193e
Bettina1011 <bw038@hdm-stuttgart.de>
2018-11-25 17:21:46 +0100

    Bootstrap Gridsystem

--- index.php
+++ index.php
@@ -10,47 +10,53 @@ include_once 'header.php';
 </head>
 <body>
 
-    <div class="container">
+
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-4">
-<br>
-    <?php
-    // schaut durch die Session, ob der Nutzer angemeldet ist
-    session_start();
-    if (isset($_SESSION["angemeldet"]))
-    {
-        echo "Hallo"." " .$_SESSION["angemeldet"];
-    }
-    else {
-        // Falls der Nutzer nicht angemeldet ist, wird er mit header auf die Login-Seite geleitet
-        header("Location:login.php");
-    }
+            <div class="col-3">
 
-    ?>
-</div>
-            <div class="container-fluid">
-            <div class="row">
-                <div class="col-6">
-                    <!-- Dies ist die Form, damit der User einen Post schreiben kann -->
-                    <form action="formular_abfrage_index.php" method="post">
-                        <textarea name="content" class="form-control" rows="3"></textarea><br>
-                        <input class="btn btn-primary" type="submit" value="Posten">
-
-                </div>
             </div>
-        </div>
-            <br>
-            <br>
-            <?php
-            // startet die Session, um zu erkennen, welcher Nutzer eingeloggt ist und vom wem er die Inhalte auf der Startseite anzeigen soll
-            session_start();
-            if (isset($_SESSION["angemeldet"])) {
-            echo "Eingeloggt ist der Benutzer " . $_SESSION['angemeldet'];
-            ?><br>
-            <br><br>
-            <div class="container">
-                <div class="row">
-                    <div class="col-4"
+
+            <div class="col-6">
+
+                <?php
+                // startet die Session, um zu erkennen, welcher Nutzer eingeloggt ist und vom wem er die Inhalte auf der Startseite anzeigen soll
+                session_start();
+                if (isset($_SESSION["angemeldet"])) {
+                echo "Eingeloggt ist der Benutzer " . $_SESSION['angemeldet'];
+                ?>
+                <br>
+                <br>
+
+
+                <?php
+                // schaut durch die Session, ob der Nutzer angemeldet ist
+                session_start();
+                if (isset($_SESSION["angemeldet"]))
+                {
+                    echo "Hallo"." " .$_SESSION["angemeldet"];
+                }
+                else {
+                    // Falls der Nutzer nicht angemeldet ist, wird er mit header auf die Login-Seite geleitet
+                    header("Location:login.php");
+                }
+
+                ?>
+                <br>
+                <br>
+
+
+
+                <!-- Dies ist die Form, damit der User einen Post schreiben kann -->
+                <form action="formular_abfrage_index.php" method="post">
+                    <textarea name="content" class="form-control" rows="3"></textarea><br>
+                    <input class="btn btn-primary" type="submit" value="Posten">
+                    <br>
+                    <br>
+
+
+
+
                     <?php
                     // stellt die Verbindung zur Datenbank her
                     include "userdata.php";
@@ -62,10 +68,11 @@ include_once 'header.php';
                         echo $content['text'] . "<br /><br />";
                     }
                     }
-                ?>
+                    ?>
             </div>
-        </div>
 
+        </div>
+    </div>
 
 </body>
 </html>
