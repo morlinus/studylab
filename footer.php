7c1a0cd37ae266a1a46fbcdf97513b683173a512
Bettina1011 <bw038@hdm-stuttgart.de>
2018-11-28 12:44:18 +0100

    Sticky-Footer Bootstrap + CSS

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+include 'userdata.php';
+?>
+
+
+<!doctype html>
+<html lang="de">
+<meta charset="utf-8">
+
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="studylab.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+
+<footer class="page-footer navbar-inverse">
+
+    <div class="footer text-center py-3">© 2018 Copyright:
+        <a href="#">StudiLAB.com</a>
+        <a href="https://mars.iuk.hdm-stuttgart.de/~as325/impressum.html">Impressum</a>
+    </div>
+
+</footer>
+
+</html>
