97e853dc06af272a0bf58ff4d3a639e8bfb7a807
Linus Morlinghaus <lm092@hdm-stuttgart.de>
2018-12-10 13:12:36 +0100

    fehler behebung

--- profil_folgen2.php
+++ profil_folgen2.php
@@ -144,10 +144,10 @@ $row_folgen = $bild_folgen->fetch();
             $bilder = $beitrags_bild->fetch();
             $dbabgleich = $bilder ["post_id"];
 
-            $id_header = $_SESSION ["id"];
-            $bild_header = $pdo->prepare("SELECT * FROM bilduplad WHERE user_id=$id_header");
-            $bild_header->execute();
-            while ($row_header = $bild_header->fetch()){
+            $id_folgen2 = $_SESSION ["id"];
+            $bild_folgen2 = $pdo->prepare("SELECT * FROM bilduplad WHERE user_id=$id_header");
+            $bild_folgen2->execute();
+            while ($row_folgen2 = $bild_folgen2->fetch()){
             ?>
 
             <div class="shadow-sm p-3 mb-5 bg-white rounded">
@@ -159,7 +159,7 @@ $row_folgen = $bild_folgen->fetch();
 
                     $beitragsersteller = $content['userid'];
 
-                    echo("<img src='data:" . $row_header['format'] . ";base64," . base64_encode($row_header['datei']) . "'width=' alt='Nutzerprofilbild' class='profilbild-navbar'>");
+                    echo("<img src='data:" . $row_folgen2['format'] . ";base64," . base64_encode($row_folgen2['datei']) . "'width=' alt='Nutzerprofilbild' class='profilbild-navbar'>");
                     }
                     ?>
                     <?php
