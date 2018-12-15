e56012e190700243ecb6be02c7ab7b139f8868d0
Bettina1011 <bw038@hdm-stuttgart.de>
2018-12-15 14:23:00 +0100

    CSS + Anpassungen

--- themenuebersicht.php
+++ themenuebersicht.php
@@ -112,20 +112,20 @@ include "header.php";
                     <h5>Ist das richtige Thema noch nicht dabei?</h5>
                     <h5>Hier kannst du danach suchen</h5>
 
-                    <!-- hier kann der User den Themen eingeben,  nach denen er suchen möchte -->
-                    <form class="form-inline my-2 my-lg-0" action="themenuebersicht.php" method="post" style="width: 32%; margin:auto;">
-                        <input class="suchfeld form-control mr-sm-2" type="text" placeholder="" name="themenuebersicht" value="" required>
+                    <!-- hier kann der User die Themen eingeben,  nach denen er suchen möchte -->
+                    <form class="form-inline my-2 my-lg-0" action="themenuebersicht.php" method="post" style="width: 32%; margin: auto;">
+                        <input class="suchfeld form-control mr-sm-2" type="text" placeholder="" name="themenuebersicht" value="">
                         <button class="suchfeld btn btn-secondary" type="submit" name="suchen" value="Suchen">Suchen</button>
                     </form>
 
                     <br>
+                </div>
+        </div>
                     <?php
                     // gibt die Daten aus, die im Formular eingeben wurden
                     if (isset($_POST['suchen'])) {
+                    ?>
 
-                        ?>
-                </div>
-        </div>
 
 
                         <h5 style="text-align: center"> <?php echo "Suchergebnisse: <br>";?> </h5>
@@ -153,12 +153,12 @@ include "header.php";
                                 $bild_suche->execute();
                                 $row_suche = $bild_suche->fetch();
 
-                                echo '<div class="col-12-ergebnisse" style="width: 40%; margin:auto;">';
+                                echo '<div class="col-12-ergebnisse" style="width: 40%; margin: auto;">';
                                 echo '<ul class="kommentar2">';
                                 //echo("<img src='data:" . $row_suche['format'] . ";base64," . base64_encode($row_suche['datei']) . "'width=' alt='Nutzerprofilbild' class='profilbild-navbar'>");
                                 echo ("<img src='https://mars.iuk.hdm-stuttgart.de/~lm092/Studylab_quadrat.png' width='' alt='Nutzerprofilbild' class='profilbild-navbar'>");
                                 echo " ";
-                                echo ('<a class="suchen-ergebnisse" href="themen.php?themen='.$themalink.'">'."Beitrag zum Thema "."#". $row['themen']);
+                                echo '<a class="suchen-ergebnisse" href="themen.php?themen='.$themalink.'">'."Beitrag zum Thema "."#". $row['themen'].'</a>';
                                 //echo "<br>". $row["name"]." ". $row ["nachname"];
                                 echo '</ul>';
                                 echo '</div>';
@@ -170,9 +170,9 @@ include "header.php";
                             echo "Keine passenden Themen gefunden";
                         }
 
-                    }
+                        }
 
-                    ?>
+                        ?>
 
 
     </div>
