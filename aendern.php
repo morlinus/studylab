64563441c1bf2ad9193ff9aa4ae60f669fc48ae3
Linus Morlinghaus <lm092@hdm-stuttgart.de>
2018-12-17 23:19:41 +0100

    ändern sicherer gemacht

--- aendern.php
+++ aendern.php
@@ -15,25 +15,27 @@ if(!isset($_POST['submit'])) exit;
 $name= $_POST["name"];
 $name2= count($name);
 if ($name2 > 0) {
-    $statement = $pdo->prepare("UPDATE studylab SET name = ?  WHERE id=$id");
-    //$statement -> bindParam ()
-    $statement->execute(array($_POST["name"]));
+    $statement = $pdo->prepare("UPDATE studylab SET name = :name  WHERE id=$id");
+    $statement -> bindParam ("name",$name);
+    $statement->execute();
 }
 // Der Nachname wird durch Post übergeben und wird dann in der Datenbank bei Veränderung überschrieben
 $nachname= $_POST["nachname"];
 
 $nachname2= count($nachname);
 if ($nachname2 > 0) {
-    $statement = $pdo->prepare("UPDATE studylab SET nachname = ?  WHERE id=$id");
-    $statement->execute(array($_POST["nachname"]));
+    $statement = $pdo->prepare("UPDATE studylab SET nachname = :nachname  WHERE id=$id");
+    $statement -> bindParam ("nachname",$nachname);
+    $statement->execute();
 }
 // Der Benutzername wird durch Post übergeben und wird dann in der Datenbank bei Veränderung überschrieben
 $benutzername= $_POST["benutzername"];
 
 $benutzername2= count($benutzername);
 if ($benutzername2 > 0) {
-    $statement = $pdo->prepare("UPDATE studylab SET benutzername = ?  WHERE id=$id");
-    $statement->execute(array($_POST["benutzername"]));
+    $statement = $pdo->prepare("UPDATE studylab SET benutzername = :benutzername  WHERE id=$id");
+    $statement -> bindParam ("benutzername",$benutzername);
+    $statement->execute();
     //Benachrichtigungsfunktion wird auf den neuen Benutzernamen aktiviert
     $insert_ben = $pdo -> prepare ("ALTER TABLE benachrichtigung ADD $benutzername VARCHAR(11) NOT NULL");
     $insert_ben -> execute ();
@@ -46,32 +48,36 @@ $geburtsdatum= $_POST["geburtsdatum"];
 
 $geburtsdatum2= count($geburtsdatum);
 if ($geburtsdatum2 > 0) {
-    $statement = $pdo->prepare("UPDATE studylab SET geburtsdatum = ?  WHERE id=$id");
-    $statement->execute(array($_POST["geburtsdatum"]));
+    $statement = $pdo->prepare("UPDATE studylab SET geburtsdatum = :geburtsdatum  WHERE id=$id");
+    $statement -> bindParam ("geburtsdatum",$geburtsdatum);
+    $statement->execute();
 }
 // Der Studiengang wird durch Post übergeben und wird dann in der Datenbank bei Veränderung überschrieben
 $studiengang= $_POST["studiengang"];
 
 $studiengang2= count($studiengang);
 if ($studiengang2 > 0) {
-    $statement = $pdo->prepare("UPDATE studylab SET studiengang = ?  WHERE id=$id");
-    $statement->execute(array($_POST["studiengang"]));
+    $statement = $pdo->prepare("UPDATE studylab SET studiengang = studiengang  WHERE id=$id");
+    $statement -> bindParam ("studiengang",$studiengang);
+    $statement->execute();
 }
 // Die E-Mail wird durch Post übergeben und wird dann in der Datenbank bei Veränderung überschrieben
 $email= $_POST["email"];
 
 $email2= count($email);
 if ($email2 > 0) {
-    $statement = $pdo->prepare("UPDATE studylab SET email = ?  WHERE id=$id");
-    $statement->execute(array($_POST["email"]));
+    $statement = $pdo->prepare("UPDATE studylab SET email = :email  WHERE id=$id");
+    $statement -> bindParam ("email",$email);
+    $statement->execute();
 }
 // Das Semester wird durch Post übergeben und wird dann in der Datenbank bei Veränderung überschrieben
 $semester= $_POST["semester"];
 
 $semester2= count($semester);
 if ($semester2 > 0) {
-    $statement = $pdo->prepare("UPDATE studylab SET semester = ?  WHERE id=$id");
-    $statement->execute(array($_POST["semester"]));
+    $statement = $pdo->prepare("UPDATE studylab SET semester = :semester  WHERE id=$id");
+    $statement -> bindParam ("semester",$semester);
+    $statement->execute();
 }
 // Der Status wird durch Post übergeben und wird dann in der Datenbank bei Veränderung überschrieben
 $status= $_POST["status"];
@@ -79,7 +85,8 @@ $status= $_POST["status"];
 $status2= count($status);
 if ($status2 > 0) {
     $statement = $pdo->prepare("UPDATE studylab SET semester = ?  WHERE id=$id");
-    $statement->execute(array($_POST["status"]));
+    $statement -> bindParam ("status",$status);
+    $statement->execute();
 }
 // Das alte und das neue Passwort wird durch Post übergeben
 $passwort_alt=$_POST["passwort_alt"];
